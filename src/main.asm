678c4a0e518e82b87a588688f49815d675438b73
Москаленко Алексей Михайлович <am_moskalenko@betboom.org>
2023-04-08 03:38:30 +0300

    Example scroll noise up

--- src/main.asm
+++ src/main.asm
@@ -6,20 +6,21 @@ loop
     ld  l,a
     ld  h,0 
 	ld	de,#4000	;; some loaded and
-	ld	bc,#1b00	;; executed code
+	ld	bc,#1800	;; executed code
     ldir
-    cp 23
-    jp z,start
     add 32
+
+    jp c,start
     jp loop
 codend
 
 	; build
 	if (_ERRORS == 0 && _WARNINGS == 0)
-        savesna SNA_FILENAME, start
-        savebin BIN_FILENAME, start, codend-start
+        page 0
         include "./include/BasicLib/BasicLoader.asm"
         savetap TAP_FILENAME, CODE, "code0", start, codend-start
         savetrd TRD_FILENAME, "code0.C" , start , codend-start
+        savesna SNA_FILENAME, start
+        savebin BIN_FILENAME, start, codend-start
         labelslist USERL_FILENAME
 	endif
